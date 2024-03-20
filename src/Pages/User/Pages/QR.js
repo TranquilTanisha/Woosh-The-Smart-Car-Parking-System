1bb74ba58c418a6b18dfcf249d92379a12c3f2a9
Siddhanth Shah <siddhanth.shah@somaiya.edu>
2024-03-20 14:32:09 +0530

    QR test mobile view

--- src/Pages/User/Pages/QR.js
+++ src/Pages/User/Pages/QR.js
@@ -1,3 +1,132 @@
+// import React, { useState, useEffect } from 'react';
+// import Navbar from '../../../Components/Navbar/Navbar';
+// import Bottombar from '../../../Components/Navbar/Bottombar';
+// import QrScanner from 'react-qr-scanner';
+// import '../../../App.css';
+
+// function QR() {
+//   const [qrResult, setQrResult] = useState(null);
+//   const [permissionGranted, setPermissionGranted] = useState(false);
+//   const [cameraErrorMessage, setCameraErrorMessage] = useState('');
+
+//   const handleScan = data => {
+//     if (data) {
+//       setQrResult(data);
+//       if (isValidURL(data.text)) {
+//         window.open(data.text, '_blank');
+//       }
+//       setPermissionGranted(false);
+//     }
+//   };
+
+//   const isValidURL = (url) => {
+//     try {
+//       new URL(url);
+//       return true;
+//     } catch (error) {
+//       return false;
+//     }
+//   };
+
+//   const handleError = err => {
+//     console.error(err);
+//   };
+
+//   useEffect(() => {
+//     const requestCameraPermission = () => {
+//       const isConfirmed = window.confirm("Allow this website to access your camera?");
+//       if (isConfirmed) {
+//         navigator.mediaDevices.enumerateDevices()
+//           .then(devices => {
+//             const rearCamera = devices.find(device => device.kind === 'videoinput' && device.label.includes('back'));
+//             if (rearCamera) {
+//               navigator.mediaDevices.getUserMedia({ video: { deviceId: rearCamera.deviceId } }) // Specify the deviceId of the rear camera
+//                 .then(() => setPermissionGranted(true))
+//                 .catch(() => {
+//                   setCameraErrorMessage('Rear camera not accessible.');
+//                 });
+//             } else {
+//               setCameraErrorMessage('Rear camera not found.');
+//             }
+//           })
+//           .catch(() => {
+//             setCameraErrorMessage('Error enumerating devices.');
+//           });
+//       }
+//     };
+    
+//     requestCameraPermission();
+//   }, []);
+
+//   const handleRequestPermissionAgain = () => {
+//     setPermissionGranted(false);
+//     setCameraErrorMessage('');
+//     const requestCameraPermission = () => {
+//       const isConfirmed = window.confirm("Allow this website to access your camera?");
+//       if (isConfirmed) {
+//         navigator.mediaDevices.enumerateDevices()
+//           .then(devices => {
+//             const rearCamera = devices.find(device => device.kind === 'videoinput' && device.label.includes('back'));
+//             if (rearCamera) {
+//               navigator.mediaDevices.getUserMedia({ video: { deviceId: rearCamera.deviceId } }) // Specify the deviceId of the rear camera
+//                 .then(() => setPermissionGranted(true))
+//                 .catch(() => {
+//                   setCameraErrorMessage('Rear camera not accessible.');
+//                 });
+//             } else {
+//               setCameraErrorMessage('Rear camera not found.');
+//             }
+//           })
+//           .catch(() => {
+//             setCameraErrorMessage('Error enumerating devices.');
+//           });
+//       }
+//     };
+//     requestCameraPermission();
+//   };
+
+//   const value = "QR";
+
+//   return (
+//     <div>
+//       <div className="navbar">
+//         <Navbar />
+//       </div>
+//       <div style={{ textAlign: 'center', position: 'relative', height: '100vh' }}>
+//         <h1>QR</h1>
+//         {permissionGranted && (
+//           <div style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', width: '100%', maxWidth: '300px' }}>
+//             <div className="scanner-animation">
+//               <QrScanner
+//                 onScan={handleScan}
+//                 onError={handleError}
+//                 style={{ width: '100%' }}
+//                 facingMode='environment'
+//               />
+//             </div>
+//           </div>
+//         )}
+//         {qrResult && <p>Scanned QR Code Content: {qrResult.text}</p>}
+//         {cameraErrorMessage && <p>{cameraErrorMessage}</p>}
+//         {!permissionGranted && !qrResult && (
+//           <button onClick={handleRequestPermissionAgain} className="camera-permission-button">
+//             Request Camera Permission
+//           </button>
+//         )}
+//       </div>
+//       <div className="bottombar">
+//         <Bottombar value={value}/>
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default QR;constraints={{facingMode: 'environment'}}
+
+
+
+
+
 import React, { useState, useEffect } from 'react';
 import Navbar from '../../../Components/Navbar/Navbar';
 import Bottombar from '../../../Components/Navbar/Bottombar';
@@ -8,6 +137,26 @@ function QR() {
   const [qrResult, setQrResult] = useState(null);
   const [permissionGranted, setPermissionGranted] = useState(false);
   const [cameraErrorMessage, setCameraErrorMessage] = useState('');
+
+  const requestCameraPermission = async () => {
+    try {
+      const devices = await navigator.mediaDevices.enumerateDevices();
+      const rearCamera = devices.find(device => device.kind === 'videoinput' && device.label.includes('back'));
+      
+      if (rearCamera) {
+        await navigator.mediaDevices.getUserMedia({ video: { deviceId: rearCamera.deviceId } });
+        setPermissionGranted(true);
+      } else {
+        setCameraErrorMessage('Rear camera not found.');
+      }
+    } catch (error) {
+      setCameraErrorMessage('Error accessing camera.');
+    }
+  };
+
+  useEffect(() => {
+    requestCameraPermission();
+  }, []);
 
   const handleScan = data => {
     if (data) {
@@ -32,56 +181,9 @@ function QR() {
     console.error(err);
   };
 
-  useEffect(() => {
-    const requestCameraPermission = () => {
-      const isConfirmed = window.confirm("Allow this website to access your camera?");
-      if (isConfirmed) {
-        navigator.mediaDevices.enumerateDevices()
-          .then(devices => {
-            const rearCamera = devices.find(device => device.kind === 'videoinput' && device.label.includes('back'));
-            if (rearCamera) {
-              navigator.mediaDevices.getUserMedia({ video: { deviceId: rearCamera.deviceId } }) // Specify the deviceId of the rear camera
-                .then(() => setPermissionGranted(true))
-                .catch(() => {
-                  setCameraErrorMessage('Rear camera not accessible.');
-                });
-            } else {
-              setCameraErrorMessage('Rear camera not found.');
-            }
-          })
-          .catch(() => {
-            setCameraErrorMessage('Error enumerating devices.');
-          });
-      }
-    };
-    
-    requestCameraPermission();
-  }, []);
-
   const handleRequestPermissionAgain = () => {
     setPermissionGranted(false);
     setCameraErrorMessage('');
-    const requestCameraPermission = () => {
-      const isConfirmed = window.confirm("Allow this website to access your camera?");
-      if (isConfirmed) {
-        navigator.mediaDevices.enumerateDevices()
-          .then(devices => {
-            const rearCamera = devices.find(device => device.kind === 'videoinput' && device.label.includes('back'));
-            if (rearCamera) {
-              navigator.mediaDevices.getUserMedia({ video: { deviceId: rearCamera.deviceId } }) // Specify the deviceId of the rear camera
-                .then(() => setPermissionGranted(true))
-                .catch(() => {
-                  setCameraErrorMessage('Rear camera not accessible.');
-                });
-            } else {
-              setCameraErrorMessage('Rear camera not found.');
-            }
-          })
-          .catch(() => {
-            setCameraErrorMessage('Error enumerating devices.');
-          });
-      }
-    };
     requestCameraPermission();
   };
 
@@ -101,7 +203,7 @@ function QR() {
                 onScan={handleScan}
                 onError={handleError}
                 style={{ width: '100%' }}
-                facingMode='environment' // Use 'environment' for back camera
+                constraints={{facingMode: 'environment'}}
               />
             </div>
           </div>
