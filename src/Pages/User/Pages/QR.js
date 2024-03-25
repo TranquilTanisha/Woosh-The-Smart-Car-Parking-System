133cae491a5ac270978b1d06f3ba38b575af4f4d
Siddhanth Shah <siddhanth.shah@somaiya.edu>
2024-03-25 11:12:37 +0530

    Fixed QR bug and updated UI

--- src/Pages/User/Pages/QR.js
+++ src/Pages/User/Pages/QR.js
@@ -1,21 +1,24 @@
 import { doc, getDoc, updateDoc } from "firebase/firestore";
 import QrScanner from 'qr-scanner';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 import '../../../App.css';
 import Bottombar from '../../../Components/Navbar/Bottombar';
 import Navbar from '../../../Components/Navbar/Navbar';
 import { auth, db } from '../../../Firebase.js';
 
-
 QrScanner.WORKER_PATH = './worker.js';
 
 const ReadQR = () => {
+  // eslint-disable-next-line
   const navigate = useNavigate();
+  // eslint-disable-next-line
   const [data, setData] = useState(null);
   const [cameraPermissionGranted, setCameraPermissionGranted] = useState(false);
   const [cameraErrorMessage, setCameraErrorMessage] = useState('');
-  const [scanning, setScanning] = useState(false); 
+  const [scanning, setScanning] = useState(false);
+  const scannerRef = useRef(null);
+
   useEffect(() => {
     const handleCameraPermission = async () => {
       try {
@@ -33,11 +36,16 @@ const ReadQR = () => {
   }, []);
 
   useEffect(() => {
-    if (data) {
-      window.location.href = data;
-    
-    }
-  }, [data, navigate]);
+    return () => {
+      if (scannerRef.current) {
+        scannerRef.current.stop();
+      }
+      const videoElement = document.getElementById('qrScanner');
+      if (videoElement) {
+        videoElement.remove();
+      }
+    };
+  }, []);
 
   const handleCameraScan = async () => {
     if (!cameraPermissionGranted) {
@@ -53,9 +61,8 @@ const ReadQR = () => {
       let constraints = { video: { facingMode: 'environment' } };
       const devices = await navigator.mediaDevices.enumerateDevices();
       const rearCamera = devices.find(device => device.kind === 'videoinput' && device.label.toLowerCase().includes('back'));
-      
+
       if (!rearCamera) {
-      
         constraints = { video: { facingMode: 'user' } };
       }
 
@@ -63,23 +70,24 @@ const ReadQR = () => {
       const stream = await navigator.mediaDevices.getUserMedia(constraints);
       const videoElement = document.createElement('video');
       videoElement.srcObject = stream;
-      videoElement.setAttribute('playsinline', 'true'); 
-      videoElement.setAttribute('id', 'qrScanner'); 
+      videoElement.setAttribute('playsinline', 'true');
+      videoElement.setAttribute('id', 'qrScanner');
       document.body.appendChild(videoElement);
-      const scanner = new QrScanner(videoElement, (result) => {
+
+      scannerRef.current = new QrScanner(videoElement, (result) => {
         if (result) {
           setData(result);
           const user = auth.currentUser;
           const uid = user.uid;
           const docRef = doc(db, "users", uid);
+
           getDoc(docRef).then(docSnap => {
             if (docSnap.exists()) {
               if (docSnap.data().entryTime) {
                 updateDoc(docRef, {
                   exitTime: new Date().toLocaleString()
                 });
-              }
-              else {
+              } else {
                 updateDoc(docRef, {
                   entryTime: new Date().toLocaleString()
                 });
@@ -89,16 +97,17 @@ const ReadQR = () => {
             }
           });
 
-          scanner.stop();
+          scannerRef.current.stop();
           videoElement.remove();
-          setScanning(false); 
+          setScanning(false);
         }
       });
-      scanner.start();
+
+      scannerRef.current.start();
     } catch (error) {
       console.error('Error accessing camera:', error);
       setCameraErrorMessage('Error accessing camera.');
-      setScanning(false); 
+      setScanning(false);
     }
   };
 
@@ -118,7 +127,7 @@ const ReadQR = () => {
               type="button"
               className="btn btn-primary mx-2 scan-button"
               onClick={handleCameraScan}
-              disabled={!cameraPermissionGranted || scanning} // Disable button while scanning
+              disabled={!cameraPermissionGranted || scanning}
             >
               {scanning ? 'Scanning...' : 'Scan QR Code'}
             </button>
@@ -133,6 +142,8 @@ const ReadQR = () => {
 };
 
 export default ReadQR;
+
+
 
 
 
